84d909e6721906ddc320758105bbcf1681d5b828
danmcCs111 <danmccs111@gmail.com>
2026-02-10 22:21:19 -0500

    update to check if directory exists before downloading

--- install/install.sh
+++ install/install.sh
@@ -2,8 +2,14 @@
 orgDir=`pwd`
 cd "$(dirname "$0")"
 cd ..
+dir="apache-maven-3.9.12"
+if [[ -d "$dir" ]]
+then
+	echo "maven exists"
+else
+	curl https://dlcdn.apache.org/maven/maven-3/3.9.12/binaries/apache-maven-3.9.12-bin.zip --output apache-maven-3.9.12-bin.zip 
+	unzip apache-maven-3.9.12-bin.zip
+fi
 
-curl https://dlcdn.apache.org/maven/maven-3/3.9.12/binaries/apache-maven-3.9.12-bin.zip --output apache-maven-3.9.12-bin.zip 
-unzip apache-maven-3.9.12-bin.zip
 
 cd $orgDir
