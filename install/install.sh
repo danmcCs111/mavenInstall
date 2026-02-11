258f98ed1f787b60a2dbd5b7a0ec451561d56ba3
danmcCs111 <danmccs111@gmail.com>
2026-02-10 22:16:29 -0500

    update install.sh to place in previous directory

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 orgDir=`pwd`
 cd "$(dirname "$0")"
+cd ..
 
 curl https://dlcdn.apache.org/maven/maven-3/3.9.12/binaries/apache-maven-3.9.12-bin.zip --output apache-maven-3.9.12-bin.zip 
 unzip apache-maven-3.9.12-bin.zip
